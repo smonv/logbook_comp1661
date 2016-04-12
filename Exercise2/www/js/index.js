e72ca6297411c441fecf28b276d756e1bd6e0317
Tien Thanh <thanhvt1609@gmail.com>
2016-04-12 22:48:18 +0700

    update exercise 2

--- Exercise2/www/js/index.js
+++ Exercise2/www/js/index.js
@@ -1,31 +1,39 @@
 function onDeviceReady() {
-    startApp();
+    $('form').on('submit', function (e) {
+        console.log('submitted');
+        e.preventDefault();
+        var name = $('#name').val();
+        var location = $('#location').val();
+        var date = $('#date').val();
+        var time = $('#time').val();
+        var organizer = $('#organizer').val();
+        var event = {
+            name: name,
+            location: location,
+            date: date,
+            time: time,
+            organizer: organizer
+        };
+
+        validateEvent(event, function (errors) {
+            if(errors.length > 0){
+                navigator.notification.alert(errors.join("\n"), null, 'Errors', 'Dismiss');
+            }
+        })
+
+    });
 }
 
-function startApp() {
-    var $$ = Dom7;
-
-    var app = new Framework7({
-        material: true,
-        cache: false,
-        modalTitle: 'madDiscovery',
-        precompileTemplates: true,
-        fastClicks: false,
-        onAjaxStart: function () {
-            app.showIndicator();
-        },
-        onAjaxComplete: function () {
-            app.hideIndicator();
-        }
-    });
-
-    var mainView = app.addView('.view-main', {});
-    app.addNotification({
-        hold: 3000,
-        closeOnClick: true,
-        message: "F7 ready"
-    });
-    app.onPageInit('index', function () {
-        console.log("init index");
-    });
+function validateEvent(event, callback) {
+    var err = [];
+    if (event.name == "") {
+        err.push("Event name is required!");
+    }
+    if (event.date == "") {
+        err.push("Event date is required!");
+    }
+    if (event.organizer == "") {
+        err.push("Event organizer is required!");
+    }
+    callback(err);
 }
